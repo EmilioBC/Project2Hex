a7f1b2b929de9631e306fad65899302b1ac25979
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-10-21 20:27:22 +0100

    init

---
+++ Project1ButtonGit/script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['red','green','blue','yellow','pink','orange'];
+
+function changeBackground() {
+    const colorIndex = parseInt(Math.random()*colors.length);
+   body.style.backgroundColor = colors[colorIndex]
+   button.style.backgroundColor = colors[colorIndex]
+}
+
+button.addEventListener('click', changeBackground);
+
+
